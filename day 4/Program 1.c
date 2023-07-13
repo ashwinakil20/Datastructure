e278df97aa8f69985a476579e7e28645c6321ff1
ashwinakil20 <139420954+ashwinakil20@users.noreply.github.com>
2023-07-13 14:42:29 +0530

    Update Program 1.c

--- day 4/Program 1.c
+++ day 4/Program 1.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-// Read only region start.
+
 int second Last Digitof (int input1)
 {
 }
-// Read only region end
-// Write code here
+
 int absolutevalue=abs(input1);
 if(absolutevalue<10)
 {
